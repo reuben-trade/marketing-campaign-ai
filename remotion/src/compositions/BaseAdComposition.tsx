7dc7c55710187c1e546197da436d0dd544fd6d33
reuben-trade <reubenjstanley@gmail.com>
2026-02-03 14:04:16 +0800

    feat: add CaptionOverlay rendering to BaseAdComposition
    - Import CaptionOverlay component for SRT-based caption display
    - Refactor renderSegment to return Fragment when caption_overlay present
    - Render captions alongside any segment type that has caption config
    - Update docstring to document caption overlay support
    
    Implements: PRD 5.3 Director Agent Refactor (s5-t12)

--- remotion/src/compositions/BaseAdComposition.tsx
+++ remotion/src/compositions/BaseAdComposition.tsx
@@ -8,6 +8,7 @@ import {
   BRollOverlay,
   TitleCard,
   SplitScreen,
+  CaptionOverlay,
   AudioTrackComponent,
 } from '../components';
 
@@ -24,6 +25,9 @@ interface Props extends AdCompositionProps {}
  * - b_roll_overlay: J-cut/L-cut with audio continuity
  * - title_card: Animated title screen with branding
  * - split_screen: Side-by-side video comparison
+ *
+ * Overlay support:
+ * - caption_overlay: SRT-based captions synced to video clips (any segment type)
  */
 export const BaseAdComposition: React.FC<Props> = ({
   timeline,
@@ -33,7 +37,6 @@ export const BaseAdComposition: React.FC<Props> = ({
 }) => {
   const renderSegment = (segment: TimelineSegment) => {
     const commonProps = {
-      key: segment.id,
       startFrame: segment.start_frame,
       durationFrames: segment.duration_frames,
       overlay: segment.overlay,
@@ -42,83 +45,114 @@ export const BaseAdComposition: React.FC<Props> = ({
       brandProfile: brand_profile,
     };
 
+    let segmentElement: React.ReactNode = null;
+
     switch (segment.type) {
       case 'video_clip':
         if (!segment.source) {
           console.warn(`[BaseAdComposition] video_clip ${segment.id} missing source`);
-          return null;
+        } else {
+          segmentElement = (
+            <VideoClipSegment
+              key={`${segment.id}-video`}
+              {...commonProps}
+              source={segment.source}
+            />
+          );
         }
-        return (
-          <VideoClipSegment
-            {...commonProps}
-            source={segment.source}
-          />
-        );
+        break;
 
       case 'text_slide':
         if (!segment.text_content) {
           console.warn(`[BaseAdComposition] text_slide ${segment.id} missing text_content`);
-          return null;
+        } else {
+          segmentElement = (
+            <TextSlideSegment
+              key={`${segment.id}-text`}
+              {...commonProps}
+              content={segment.text_content}
+            />
+          );
         }
-        return (
-          <TextSlideSegment
-            {...commonProps}
-            content={segment.text_content}
-          />
-        );
+        break;
 
       case 'generated_broll':
         if (!segment.generated_source) {
           console.warn(`[BaseAdComposition] generated_broll ${segment.id} missing generated_source`);
-          return null;
+        } else {
+          segmentElement = (
+            <BRollSegment
+              key={`${segment.id}-broll`}
+              {...commonProps}
+              source={segment.generated_source}
+            />
+          );
         }
-        return (
-          <BRollSegment
-            {...commonProps}
-            source={segment.generated_source}
-          />
-        );
+        break;
 
       case 'b_roll_overlay':
         if (!segment.broll_overlay_source) {
           console.warn(`[BaseAdComposition] b_roll_overlay ${segment.id} missing broll_overlay_source`);
-          return null;
+        } else {
+          segmentElement = (
+            <BRollOverlay
+              key={`${segment.id}-broll-overlay`}
+              {...commonProps}
+              source={segment.broll_overlay_source}
+            />
+          );
         }
-        return (
-          <BRollOverlay
-            {...commonProps}
-            source={segment.broll_overlay_source}
-          />
-        );
+        break;
 
       case 'title_card':
         if (!segment.title_card_content) {
           console.warn(`[BaseAdComposition] title_card ${segment.id} missing title_card_content`);
-          return null;
+        } else {
+          segmentElement = (
+            <TitleCard
+              key={`${segment.id}-title`}
+              {...commonProps}
+              content={segment.title_card_content}
+            />
+          );
         }
-        return (
-          <TitleCard
-            {...commonProps}
-            content={segment.title_card_content}
-          />
-        );
+        break;
 
       case 'split_screen':
         if (!segment.split_screen_content) {
           console.warn(`[BaseAdComposition] split_screen ${segment.id} missing split_screen_content`);
-          return null;
+        } else {
+          segmentElement = (
+            <SplitScreen
+              key={`${segment.id}-split`}
+              {...commonProps}
+              content={segment.split_screen_content}
+            />
+          );
         }
-        return (
-          <SplitScreen
-            {...commonProps}
-            content={segment.split_screen_content}
-          />
-        );
+        break;
 
       default:
         console.warn(`[BaseAdComposition] Unknown segment type: ${segment.type}`);
-        return null;
     }
+
+    // If segment has caption overlay, render it alongside the segment
+    if (segment.caption_overlay) {
+      return (
+        <React.Fragment key={segment.id}>
+          {segmentElement}
+          <CaptionOverlay
+            key={`${segment.id}-caption`}
+            config={segment.caption_overlay}
+            startFrame={segment.start_frame}
+            durationFrames={segment.duration_frames}
+            brandProfile={brand_profile}
+          />
+        </React.Fragment>
+      );
+    }
+
+    return segmentElement;
   };
 
   return (
